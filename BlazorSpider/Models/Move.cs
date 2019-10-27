435801fc6af40b873503bade0d665a745900f0ed
Richard Bain <rbain@iowatelecom.net>
2019-10-27 14:14:24 -0500

    initial commit

---
+++ BlazorSpider/Models/Move.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSpider.Models
+{
+    public class Move
+    {
+        public int FromPileId { get; set; }
+        public int ToPileId { get; set; }
+        public bool ToCompletedPile { get; set; }
+        public bool TurnFaceDown { get; set; }
+        public int Cards { get; set; }
+    }
+}
---
+++ BlazorSpider/Models/PlayingCardModel.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorSpider.Models
+{
+    public class PlayingCardModel
+    {
+        private const string CardIds = "A23456789TJQK";
+        public int Id { get; set; }
+        public int Value { get; set; }
+        public Suit Suit { get; set; }
+        public string Description { get { return IsFaceDown ? "Back of Card" : CardIds.Substring(Value-1, 1) + " " + Suit.ToString(); } }
+        public string ImageUrl { get { return IsFaceDown ? "Images/2B.svg" : "Images/" + CardIds.Substring(Value - 1, 1) + this.Suit.ToString().Substring(0,1) + ".svg"; } }
+        public int PileId { get; set; }
+        public int DealId { get; set; }
+        public int CompletedId { get; set; }
+        public int PileSequence { get; set; }
+        public bool IsFaceDown { get; set; }
+    }
+}
+
+public enum Suit
+{
+    Spade,
+    Heart,
+    Club,
+    Diamond
+}
